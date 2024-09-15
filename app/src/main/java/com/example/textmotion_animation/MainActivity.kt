299bdee710241d08bf3e79842292a073a48ec3de
Abdullahkhanspn <abdullahkhanspn1@gmail.com>
2024-09-15 10:18:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/textmotion_animation/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.textmotion_animation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.style.TextMotion
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.textmotion_animation.ui.theme.TextMotion_AnimationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TextMotion_AnimationTheme {
+                SmoothAnimateText() // Use the composable here
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SmoothAnimateText() {
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite transition")
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 8f,
+        animationSpec = infiniteRepeatable(tween(1000), RepeatMode.Reverse),
+        label = "scale"
+    )
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "Hello",
+            modifier = Modifier
+                .graphicsLayer {
+                    scaleX = scale
+                    scaleY = scale
+                    transformOrigin = TransformOrigin.Center
+                }
+                .align(Alignment.Center),
+            style = LocalTextStyle.current.copy(textMotion = TextMotion.Animated)
+        )
+    }
+}
